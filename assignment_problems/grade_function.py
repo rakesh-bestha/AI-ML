aac3204927fdb99b3ac5f7afddc0fce187a331cf
Rakesh Bestha <120659332+rakesh-bestha@users.noreply.github.com>
2026-02-21 12:41:00 +0530

    Update grade_function.py

--- assignment_problems/grade_function.py
+++ assignment_problems/grade_function.py
@@ -1,1 +1,9 @@
+def calculate_grade(*score, **adjustment):
+    average = sum(score)/len(score)
+    bonus = sum(adjustment.values())
+    return average + bonus
 
+grade1 = calculate_grade(85, 90,78)
+print(f"Final Grade: {grade1:.2f}%")
+grade2 = calculate_grade(70,65,80,attandnance = 5, project = 10)
+print(f"Final Grade: {grade2:.2f}%")
